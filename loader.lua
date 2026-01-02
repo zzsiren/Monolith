41dc0314773d699d1d4e1a9a0d172f0d379d04c8
zzsiren <f5b7m6scs5@privaterelay.appleid.com>
2026-01-02 12:24:47 +0545

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+--[ Discord.gg/monolith.cc ]--
+
+if not game:IsLoaded() then game.Loaded:Wait() end
+
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/8bcb8aaebb2acc7204ea193ff502890abe1c4e080e372281aed6dbf4ecaa0aa3/download"))()
